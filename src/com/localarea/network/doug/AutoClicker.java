34adca4f5caed8a9bd65cd864f6c1cc559725b74
Doug <tikiman92@gmail.com>
2019-02-07 22:02:05 -0800

    Create LICENSE (#25)
    * Create LICENSE
    
    * Update MainFrame.java
    Add license
    
    * Update AutoClicker.java
    Add license to AutoClicker.java
    
    * Update Driver.java
    add license to Driver.java
    
    * Fixes #15

--- src/com/localarea/network/doug/AutoClicker.java
+++ src/com/localarea/network/doug/AutoClicker.java
@@ -12,6 +12,22 @@
  * [10/11/13] - get and save mouse location with hotkey F8
  * To Do:
  *   - see github issues
+ * 
+ * 
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2011 Douglas Chidester
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  */
 
 package com.localarea.network.doug;
